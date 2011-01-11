b75c3572cef23683f49fef5bc7041051099e05bf
Jason Goecke <jason@goecke.net>
2011-01-10 18:53:54 -0800

    Bumped the version

--- examples/list_applications.rb
+++ examples/list_applications.rb
@@ -10,10 +10,10 @@ provisioning = TropoProvisioning.new(config['tropo']['username'], config['tropo'
 p provisioning.applications
 
 # Then you may iterate through all of your configured applications
-# provisioning.applications.each do |app|
-#   p app
-#   puts '*'*10
-# end
+provisioning.applications.each do |app|
+  p app
+  puts '*'*10
+end
 
 # Now list a single application
-# p provisioning.application(app_details.application_id)
+p provisioning.application(app_details.application_id)
--- examples/list_user.rb
+++ examples/list_user.rb
@@ -9,7 +9,7 @@ config = YAML.load(File.open('examples/config.yml'))
 provisioning = TropoProvisioning.new(config['tropo']['username'], config['tropo']['password'], :base_uri => 'http://api-smsified-eng.voxeo.net/v1')
 
 # Create an account
-p provisioning.user('54238')
-p provisioning.user('jason')
-p provisioning.search_users('username=foobar')
+#p provisioning.user('54238')
+p provisioning.user('janedoe1')
+#p provisioning.search_users('username=foobar')
 
